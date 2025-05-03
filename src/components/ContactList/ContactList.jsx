4b5bff6ec1352c3f8aeef55081ac3f6186489862
Sasha <malinka15117kk@gmail.com>
2025-05-03 12:41:41 +0300

    hm3

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,5 +1,5 @@
-import Contact from '../Contact/Contact';
 import css from './ContactList.module.css';
+import Contact from '../Contact/Contact';
 
 const ContactList = ({ contacts, onDelete }) => {
   return (
